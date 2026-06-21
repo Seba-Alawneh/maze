991d5fb4bf8639f5d63405fcd353c994a8aeea2f
sse205al-sys <sse205.al@gmail.com>
2026-06-21 20:02:30 +0300

    config1

--- config_loader.py
+++ config_loader.py
@@ -9,9 +9,10 @@ class Config:
         self.OUTPUT_FILE = ""
         self.PERFECT = False
         self._load(filename)
+        self.validate_data()
 
     def _load(self, filename: str):
-        with open (filename , "r") as f:
+        with open(filename, "r") as f:
             for line in f:
                 line = line.strip()
                 if line.startswith("#") or line == "":
@@ -30,6 +31,26 @@ class Config:
                     x, y = value.split(",")
                     self.EXIT = (int(x), int(y))
                 elif key == "OUTPUT_FILE":
-                     self.OUTPUT_FILE = value
+                    self.OUTPUT_FILE = value
                 elif key == "PERFECT":
                     self.PERFECT = value.lower() == "true"
+
+    def validate_data(self):
+        if self.WIDTH <= 0 or self.HEIGHT <= 0:
+            raise ValueError("WIDTH and HEIGHT must be greater than 0.")
+
+        if self.ENTRY == self.EXIT:
+            raise ValueError("ENTRY and EXIT points must be different.")
+
+        if (self.ENTRY[0] < 0 or self.ENTRY[0] >= self.WIDTH or
+                self.ENTRY[1] < 0 or self.ENTRY[1] >= self.HEIGHT):
+            raise ValueError("ENTRY point must be inside the maze bounds.")
+
+        if (self.EXIT[0] < 0 or self.EXIT[0] >= self.WIDTH or
+                self.EXIT[1] < 0 or self.EXIT[1] >= self.HEIGHT):
+            raise ValueError("EXIT point must be inside the maze bounds.")
+
+        if self.OUTPUT_FILE == "":
+            raise ValueError(
+                "OUTPUT_FILE is missing in the configuration."
+            )
